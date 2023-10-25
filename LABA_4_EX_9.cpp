c8e97c7c10e2a516b6bc8a9d6de2ac8b542190ef
RomeroNX <114654445+RomeroNX@users.noreply.github.com>
2023-10-25 10:09:56 +0300

    Add files via upload

---
+++ LABA_4_EX_9.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+int main()
+{
+	int num1, num2, num3, num4;
+
+	std::cout << "Enter the three numbers: ";
+	std::cin >> num1 >> num1 >> num3 >> num4;
+
+	// Check if each number is in the range 1-255
+	if (num1 >= 1 && num1 <= 255 && num2 >= 1 && num2 <= 255 && num3 >= 1 && num3 <= 255 && num4 >= 1 && num4 <= 255)
+	{
+		std::cout << num1 << "." << num2 << "." << num3 << "." << num4 << std::endl;
+	}
+	else
+	{
+		std::cout << "Invalid input, Each number must be in this range (1-255)" << std::endl;
+	}
+
+	return 0;
+}
